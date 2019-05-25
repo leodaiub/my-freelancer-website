e6d42c8f900bdc29cca9b4a9fc7049c47d224b84
leodaiub <leodaiub@gmail.com>
2019-05-25 01:35:10 -0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-ECHO est  ativado.
+const menuIcon = document.getElementById('menu-icon');
+const slideoutMenu = document.getElementById('slide-out-menu');
+const searchIcon = document.getElementById('search-icon');
+const searchBox = document.getElementById('search-box');
+
+searchIcon.addEventListener('click', function() {
+    if(searchBox.style.top == '72px'){
+        searchBox.style.top = '24px';
+        searchBox.style.pointerEvents = 'none';
+    } else {
+        searchBox.style.top = '72px';
+        searchBox.style.pointerEvents = 'auto';
+    }
+});
+
+menuIcon.addEventListener('click', function(){
+    if(slideoutMenu.style.opacity == "1") {
+        slideoutMenu.style.opacity = '0';
+        slideoutMenu.style.pointerEvents = 'none';
+    } else {
+        slideoutMenu.style.opacity = '1';
+        slideoutMenu.style.pointerEvents = 'auto';
+    }
+});
